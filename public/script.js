d7dc298560ebc783ef8928c0caad363ceae1c6d2
Jerry Applegart <applegarth1630@gmail.com>
2023-04-24 11:00:27 -0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// if variables are boxes then objects are files cabinets
+const colors = {
+    red: "red",
+    blue: "blue",
+    green: "green",
+    yellow: "yellow",
+    purple: "purple",
+    orange: "orange",
+
+}
